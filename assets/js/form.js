55edc4a4e27c8adaac9bdf2afa007a4dd4187927
Joseph Ruff <joegruff16@gmail.com>
2024-03-23 18:21:42 -0400

    Updated the style of both pages and started the JS for the form to store it in local storage after the submit buttom is pressed

---
+++ assets/js/form.js
@@ -0,0 +1,30 @@
+const userNameInput = document.querySelector('#username');
+const titleInput = document.querySelector('#title');
+const contentInput = document.querySelector('#content');
+const submitButton = document.querySelector('#submit');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+submitButton.addEventListener('click', function (event) {
+    event.preventDefault();
+)
