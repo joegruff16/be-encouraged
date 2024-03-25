2b9102e6167910145a2c366898ea036ecb7f915a
Joseph Ruff <joegruff16@gmail.com>
2024-03-24 22:00:24 -0400

    Added error message in HTML for form and adjusted code in JS for message to display

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,7 +1,7 @@
 // We are taking the username, title, and content that is inputed to be stored in the local storage
 // after the user clicks the submit button
 
-const userNameInput = document.querySelector('#uname');
+const userNameInput = document.querySelector('#username');
 const titleInput = document.querySelector('#title');
 const contentInput = document.querySelector('#content');
 const messageDiv = document.querySelector('#message');
@@ -13,22 +13,26 @@ function displayMessage(type, message) {
     messageDiv.setAttribute('class', type);
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // Input > Dowork > Output - The input is the event
 submitButton.addEventListener('click', function (event) {
-    console.log(event)
+    // console.log(event);
     event.preventDefault();
+
+    const userNameInput = userNameInput.value;
+    const titleInput = titleInput.value;
+    const contentInput = contentInput.value;
+
+    if (username === '') {
+        displayMessage('error', 'Username cannot be blank');
+    } else if (title === '') {
+        displayMessage('error', 'Title cannot be blank');
+    } else if (content === '') {
+        displayMessage('error', 'Content cannot be blank');
+    } else {
+        displayMessage('success', 'Logged in successfully');
+
+        localStorage.setItem('email', email);
+        localStorage.setItem('password', password);
+        renderLastRegistered();
+    }
 })
