83d6973401ede0d9de66c39becec3a3449cde503
Joseph Ruff <joegruff16@gmail.com>
2024-03-24 21:31:17 -0400

    Attached my JS to HTML and added display message function to JS

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,8 +1,17 @@
-const userNameInput = document.querySelector('#username');
+// We are taking the username, title, and content that is inputed to be stored in the local storage
+// after the user clicks the submit button
+
+const userNameInput = document.querySelector('#uname');
 const titleInput = document.querySelector('#title');
 const contentInput = document.querySelector('#content');
+const messageDiv = document.querySelector('#message');
 const submitButton = document.querySelector('#submit');
 
+// Message Display after submit button is selected
+function displayMessage(type, message) {
+    messageDiv.textContent = message;
+    messageDiv.setAttribute('class', type);
+}
 
 
 
@@ -18,13 +27,8 @@ const submitButton = document.querySelector('#submit');
 
 
 
-
-
-
-
-
-
-
+// Input > Dowork > Output - The input is the event
 submitButton.addEventListener('click', function (event) {
+    console.log(event)
     event.preventDefault();
-)
+})
