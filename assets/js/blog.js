fb15a36c877b127b4e750b77c124e4f4621b7650
Joseph Ruff <joegruff16@gmail.com>
2024-03-27 19:47:08 -0400

    Completed JSON parse array for blog html page through blog JS

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,3 +1,36 @@
 // Include function back button
 
 // Function that retrieves data from local storage
+const blogEl = document.querySelector(".blogPosts")
+function displayBlogs(event) {
+    event.preventDefault();
+
+    const blogs = JSON.parse(localStorage.getItem("blogPost")) || [];
+    console.log(blogs)
+    let blogTable = "";
+    for (var i = 0; i < blogs.length; i++) {
+        blogTable += `
+        <table class="table">
+            <tr>
+                <th>
+                    ${blogs[i].title}
+                </th>
+            </tr>
+            <tr>
+                <td>
+                    ${blogs[i].content}
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    posted by: ${blogs[i].username}
+                </td>
+                <br>
+            </tr>
+        </table>
+        `;
+        blogEl.innerHTML = blogTable;
+    }
+
+}
+document.addEventListener("DOMContentLoaded", displayBlogs)
--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,1 +1,15 @@
 // Include light and dark mode toggle function
+const toggleButton = document.querySelector('#submit');
+
+
+// Input > Dowork > Output - The input is the event
+toggleButton.addEventListener('click', function (event) {
+    // console.log(event);
+    event.preventDefault();
+    console.log(`does this work?`);
+})
+
+function lightDarkMode() {
+    var element = document.body;
+    element.classList.toggle("light-dark");
+}
